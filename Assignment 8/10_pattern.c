e133802c41c122db9cd90bbd21286088059f224c
Gourav Chalotra <90745717+gourav-chalotra@users.noreply.github.com>
2022-11-17 23:26:43 +0530

    Update 10_pattern.c

--- Assignment 8/10_pattern.c
+++ Assignment 8/10_pattern.c
@@ -1,1 +1,40 @@
 // print the following
+// 1 2 3 4 3 2 1
+// 1 2 3   3 2 1
+// 1 2       2 1
+// 1           1
+
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        int n = 1;
+        for (int j = 1; j <= 4 * 2 - 1; j++)
+        {
+            if (j <= 4 - i + 1)
+            {
+
+                printf("%d", n);
+                n++;
+            }
+            else if (n == 5)
+            {
+                n = n - 2;
+                printf("%d", n);
+            }
+            else if (j >= 4 + i - 1)
+            {
+                n--;
+                printf("%d", n);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
